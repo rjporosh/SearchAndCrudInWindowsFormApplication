08ec32d689f29ce2a2af21b6598b8cb816a78df3
rjporosh <porosh19940423@gmail.com>
2018-11-08 18:41:45 +0600

    first commit

---
+++ zxctext/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace zxctext
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPersonName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.AllowUserToDeleteRows = false;
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.uname,
+            this.type,
+            this.pName,
+            this.num});
+            this.dgvMain.Location = new System.Drawing.Point(7, 119);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.ReadOnly = true;
+            this.dgvMain.Size = new System.Drawing.Size(522, 220);
+            this.dgvMain.TabIndex = 0;
+            this.dgvMain.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMain_CellContentDoubleClick);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "Id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // uname
+            // 
+            this.uname.DataPropertyName = "UserName";
+            this.uname.HeaderText = "User Name";
+            this.uname.Name = "uname";
+            this.uname.ReadOnly = true;
+            // 
+            // type
+            // 
+            this.type.DataPropertyName = "Type";
+            this.type.HeaderText = "Type";
+            this.type.Name = "type";
+            this.type.ReadOnly = true;
+            // 
+            // pName
+            // 
+            this.pName.DataPropertyName = "PersonName";
+            this.pName.HeaderText = "Name";
+            this.pName.Name = "pName";
+            this.pName.ReadOnly = true;
+            // 
+            // num
+            // 
+            this.num.DataPropertyName = "PhoneNumber";
+            this.num.HeaderText = "Phone Number";
+            this.num.Name = "num";
+            this.num.ReadOnly = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(535, 248);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(626, 248);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(717, 248);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(627, 90);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 4;
+            this.txtUserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPersonName
+            // 
+            this.txtPersonName.Location = new System.Drawing.Point(627, 116);
+            this.txtPersonName.Name = "txtPersonName";
+            this.txtPersonName.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonName.TabIndex = 4;
+            this.txtPersonName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(627, 152);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNumber.TabIndex = 4;
+            this.txtPhoneNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(627, 178);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(100, 20);
+            this.txtType.TabIndex = 4;
+            this.txtType.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(559, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(548, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Person Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Phone Number :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(578, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Type :";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(32, 89);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(409, 20);
+            this.txtSearch.TabIndex = 6;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Search";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(613, 316);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 509);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtPersonName);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvMain);
+            this.MaximumSize = new System.Drawing.Size(875, 548);
+            this.MinimumSize = new System.Drawing.Size(875, 548);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPersonName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ zxctext/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zxctext
+{
+
+    public partial class Form1 : Form
+    {
+        tblMain model = new tblMain();
+        //Connection String  
+        string cs = "data source=mylaptop\\sqlexpress;initial catalog=MultipleLogin;integrated security=True;multipleactiveresultsets=True;application name=EntityFramework;multisubnetfailover=False";
+        SqlConnection con;
+        SqlDataAdapter adapt;
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+            Clear();
+            PopulateDataGridView();
+        }
+
+      
+        void Clear()
+        {
+            txtUserName.Text = txtPersonName.Text = txtPhoneNumber.Text = txtType.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.Id = 0;
+        }
+
+        private void dgvMain_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvMain.CurrentRow.Index != -1)
+            {
+                model.Id = Convert.ToInt32(dgvMain.CurrentRow.Cells["Id"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    model = db.tblMains.Where(x => x.Id == model.Id).FirstOrDefault();
+                    txtUserName.Text = model.UserName;
+                    txtPersonName.Text = model.PersonName;
+                    txtPhoneNumber.Text = model.PhoneNumber;
+                    txtType.Text = model.Type;
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+        void PopulateDataGridView()
+        {
+            dgvMain.AutoGenerateColumns = false;
+            using (DBEntities db = new DBEntities())
+            {
+                dgvMain.DataSource = db.tblMains.ToList();
+
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure to Delete this Record ?", txtUserName.Text + "'s Phonebook", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.tblMains.Attach(model);
+                    db.tblMains.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            model.UserName = txtUserName.Text;
+            model.PersonName = txtPersonName.Text;
+            model.PhoneNumber = txtPhoneNumber.Text;
+            model.Type = txtType.Text;
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.Id == 0) //Insert
+                {
+                    db.tblMains.Add(model);
+                    db.SaveChanges();
+                    MessageBox.Show("Added New Contact.");
+                }
+
+                else //Update
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    MessageBox.Show("Updated Successfully");
+                }
+                    
+            }
+            Clear();
+            PopulateDataGridView();
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            model.UserName = txtUserName.Text;
+            model.PersonName = txtPersonName.Text;
+            model.PhoneNumber = txtPhoneNumber.Text;
+            model.Type = txtType.Text;
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.Id != 0) //Update
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    MessageBox.Show("Updated Successfully");
+                }
+            }
+        }
+
+       
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            con = new SqlConnection(cs);
+            con.Open();
+            
+            //    SearchDirectionHint by Person Name Only
+           adapt = new SqlDataAdapter("select * from tblMain where PersonName like '" + txtSearch.Text + "%'", con);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            dgvMain.DataSource = dt;
+            con.Close();
+        }
+    }
+}
